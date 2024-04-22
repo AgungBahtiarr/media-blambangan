4f4009511e9542b72ed0b707169a1b0fe5233057
AgungBahtiarr <agungbahtiar050@gmail.com>
2024-04-22 10:16:36 +0700

    fix

--- tina/__generated__/client.ts
+++ tina/__generated__/client.ts
@@ -1,5 +1,5 @@
 import { createClient } from "tinacms/dist/client";
 import { queries } from "./types";
-export const client = createClient({ url: 'http://localhost:4001/graphql', token: '765f170408e3d2e3d8301dee07049fd6dfb31b59', queries,  });
+export const client = createClient({ url: 'https://content.tinajs.io/1.4/content/4dcc82f5-6a9e-4a7c-976a-5c553c4bafea/github/master', token: '765f170408e3d2e3d8301dee07049fd6dfb31b59', queries,  });
 export default client;
   
