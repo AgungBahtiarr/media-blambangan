859765a813a3e761d0e65b5618dfd809784936fc
AgungBahtiarr <agungbahtiar050@gmail.com>
2024-04-22 10:10:26 +0700

    generate tina

--- tina/__generated__/client.ts
+++ tina/__generated__/client.ts
@@ -1,8 +1,5 @@
-import { createClient } from 'tinacms/dist/client'
-import { queries } from './types'
-export const client = createClient({
-	url: 'http://localhost:4001/graphql',
-	token: '34fe22547cbbafa8b8d995fe440a4a3dda72e2bc',
-	queries
-})
-export default client
+import { createClient } from "tinacms/dist/client";
+import { queries } from "./types";
+export const client = createClient({ url: 'http://localhost:4001/graphql', token: '765f170408e3d2e3d8301dee07049fd6dfb31b59', queries,  });
+export default client;
+  
